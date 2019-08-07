422d9b5caa16480ee1778318238e6def82cb9d7a
lssbcs <leosb@theodo.co.uk>
2019-08-07 18:02:18 +0100

    add docs for 0.0.9

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,7 @@ author = u"Leo Siddall-Butchers"
 # The short X.Y version
 version = u""
 # The full version, including alpha/beta/rc tags
-release = u"0.0.6"
+release = u"0.0.9"
 
 
 # -- General configuration ---------------------------------------------------
