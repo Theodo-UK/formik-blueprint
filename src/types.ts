68ec752d38940af128855744c46a5e07c254717a
Theodo <leosb@theodo.co.uk>
2019-10-19 16:46:26 +0100

    create branch

--- src/types.ts
+++ src/types.ts
@@ -3,3 +3,4 @@ import { FastFieldProps, FieldProps, FieldArrayRenderProps } from 'formik';
 export type FieldTypes = FastFieldProps | FieldProps;
 
 export type FieldArrayTypes = FieldArrayRenderProps;
+
